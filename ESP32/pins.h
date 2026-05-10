27c89871c9c78bcf2ba61bc6f04fc8b17760dfff
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-10 03:20:41 -0300

    [ESP32] chore: mapear pinos de LEDs, transistores e LDR

---
+++ ESP32/pins.h
@@ -0,0 +1,17 @@
+#ifndef PINS_H
+#define PINS_H
+
+
+// Pinos
+
+#define LED1 16
+#define LED2 18
+#define LED3 21
+
+#define TRAN1 17
+#define TRAN2 19
+#define TRAN3 22
+
+#define LDR 34
+
+#endif
