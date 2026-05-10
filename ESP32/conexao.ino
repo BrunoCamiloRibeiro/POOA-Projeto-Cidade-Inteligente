5d45a2b0b6a5bce6d56e8c3dcb006262f501c2ba
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-10 03:17:31 -0300

    [ESP32] feat: implementar conexao Wi-Fi e reconexao MQTT

---
+++ ESP32/conexao.ino
@@ -0,0 +1,37 @@
+void conexaoWiFi()
+{
+  Serial.printf("Conectando a %s ", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println(" CONECTADO!");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload, length);
+
+  String acao = doc["acao"];
+
+  // ações
+}
+
+void reconectar()
+{
+  while (!client.connected()) {
+    Serial.print("Tentando conectar ao broker no Raspberry Pi...");
+    String clientId = "ESP32_LogicaTempo";
+    
+    if (client.connect(clientId.c_str())) {
+      Serial.println(" Conectado ao MQTT!");
+    } else {
+      Serial.print(" Falhou, erro: ");
+      Serial.print(client.state());
+      Serial.println(" Tentando novamente em 5s...");
+      delay(5000);
+    }
+  }
+}
+
